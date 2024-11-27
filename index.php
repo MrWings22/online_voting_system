85a226266c25a80a46902504118dbb85da221a51
Albin Thomas <127404792+MrWings22@users.noreply.github.com>
2024-11-27 19:56:43 +0530

    add admin pages to on folder

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'headerindex.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Voting System</title>
-    <link rel="stylesheet" href="css/main.css">
+
 
 </head>
 <body>
